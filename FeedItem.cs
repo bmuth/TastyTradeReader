354a8911c26b33957a49e6f4d9d88507518f1618
Brian Muth <bmuth@telus.net>
2015-10-10 14:18:38 -0700

    Initial

---
+++ FeedItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TastyTradeReader
+{
+    public class Feed : List<FeedItem>
+    { }
+
+    public class FeedItem
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Movie { get; set; }
+        public string PubDate { get; set; }
+
+        public override string ToString ()
+        {
+            return String.Format ("{0} {1}", Title.Substring (0, 20), PubDate);
+        }
+
+    }
+}
